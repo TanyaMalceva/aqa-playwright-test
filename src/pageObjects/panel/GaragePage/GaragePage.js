2bd8fb1274bda213b6b7a74040ff7c16f52fc788
Tanya <malceva.great@gmail.com>
2023-12-15 22:55:19 +0200

    Add new workflow

--- src/pageObjects/panel/GaragePage/GaragePage.js
+++ src/pageObjects/panel/GaragePage/GaragePage.js
@@ -1,8 +1,8 @@
-import BasePage from '../../BasePage.js';
+import BasePage from "../../BasePage.js";
 
 export default class GaragePage extends BasePage {
     constructor(page) {
-        super(page, '/panel/garage', page.locator('button', {hasText: 'Add car'}));
-        this.addCarButton = page.locator('button.btn.btn-primary');
+        super(page, '/panel/garage', page.locator('app-panel-layout' , {has : page.locator('button', {hasText: 'Add car'})}));
+        this.addCarBtn = page.locator('button', {hasText: 'Add car'})
     }
 }
